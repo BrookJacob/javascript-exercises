aebf8b94d186c04e0cdb4c9d088686a6cf6d4df2
Jacob Brook <brookjacob@gmail.com>
2022-03-20 23:54:18 -0600

    Add js for solution to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const palindromes = function(word) {
+    word = word.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "").replace(/\s/g, '').toLowerCase();
+    let halfWordNum = Math.floor(word.length / 2);
+    let lastHalf = word.slice(-halfWordNum);
+    let reversedLastHalf = '';
+
+    for (let i = lastHalf.length - 1; i >= 0; i--) {
+        reversedLastHalf += lastHalf[i];
+    }
+
+    console.log(lastHalf);
+    console.log(reversedLastHalf);
+    console.log(word.slice(0, halfWordNum));
+
+    if (reversedLastHalf == word.slice(0, halfWordNum)) {
+        return true;
+    } else {
+        return false;
+    }
+
 
 };
 
