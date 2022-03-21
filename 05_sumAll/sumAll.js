be97c3ad6e6fa0cb47aa3e5592409475a29f1063
Jacob Brook <brookjacob@gmail.com>
2022-03-20 21:31:49 -0600

    Add js for solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    argumentArray = Array.from(arguments);
+    argumentArray.sort();
+    if (!Number.isInteger(argumentArray[0]) || !Number.isInteger(argumentArray[1])) {
+        return 'ERROR';
+    } else if (Math.sign(argumentArray[0]) < 0 || Math.sign(argumentArray[1]) < 0) {
+        return 'ERROR';
+    } else {
+
+        let sumTotal = 0;
+        for (i = argumentArray[0]; i <= argumentArray[1]; i++) {
+            sumTotal += i;
+        }
+        return sumTotal;
+    }
 
 };
 
