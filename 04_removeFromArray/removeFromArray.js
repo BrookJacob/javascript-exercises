a16eb900f060d5d9efc99364985988a6d6ec76fd
Jacob Brook <brookjacob@gmail.com>
2022-03-20 21:18:15 -0600

    Add js for solution to 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+    let removalArray = Array.from(arguments);
+    removalArray.shift();
+    console.log(removalArray);
 
+    let filteredArray = [];
+
+    for (i = 0; i < array.length; i++) {
+        if (!removalArray.includes(array[i])) {
+            filteredArray.push(array[i]);
+        }
+    }
+    return filteredArray;
 };
 
 // Do not edit below this line
