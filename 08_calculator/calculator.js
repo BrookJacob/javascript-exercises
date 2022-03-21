7719c129b2eb8acaff8251f6a92f2789a211d481
Jacob Brook <brookjacob@gmail.com>
2022-03-20 22:51:31 -0600

    Add js for solution to 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,52 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+    return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+    let sumTotal = 0;
+
+    for (i = 0; i < sumArray.length; i++) {
+        sumTotal += sumArray[i];
+    }
+
+    return sumTotal;
 };
 
-const multiply = function() {
+const multiply = function(multiplyArray) {
+    let product = 1;
 
+    for (i = 0; i < multiplyArray.length; i++) {
+        product *= multiplyArray[i];
+    }
+
+    return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+    return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+    let product = 1;
+
+    for (i = num1; i > 0; i--) {
+        product *= i;
+    }
+
+    return product;
+
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
