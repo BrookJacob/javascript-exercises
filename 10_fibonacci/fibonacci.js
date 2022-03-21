bb38172133f91db9f280e36d4b18130894f4a110
Jacob Brook <brookjacob@gmail.com>
2022-03-21 00:11:30 -0600

    Add js for solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciTarget) {
+    fibonacciTarget = parseInt(fibonacciTarget);
+    let fibonacciSequence = [];
 
+    if (Math.sign(fibonacciTarget) != -1) {
+        for (let i = 0; i < fibonacciTarget; i++) {
+            if (i == 0 || i == 1) {
+                fibonacciSequence.push(1)
+            } else {
+                fibonacciSequence.push(fibonacciSequence[i - 1] + fibonacciSequence[i - 2])
+            }
+        }
+        return fibonacciSequence.pop();
+
+    } else {
+        return 'OOPS';
+    }
 };
 
 // Do not edit below this line
