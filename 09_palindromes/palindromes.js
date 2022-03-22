b6d7a3b1fd6554b226bafc298c5220102cbc192a
Jacob Brook <brookjacob@gmail.com>
2022-03-21 23:51:35 -0600

    Update to use built-in array and string methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,22 +1,7 @@
 const palindromes = function(word) {
     word = word.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "").replace(/\s/g, '').toLowerCase();
-    let halfWordNum = Math.floor(word.length / 2);
-    let lastHalf = word.slice(-halfWordNum);
-    let reversedLastHalf = '';
-
-    for (let i = lastHalf.length - 1; i >= 0; i--) {
-        reversedLastHalf += lastHalf[i];
-    }
-
-    console.log(lastHalf);
-    console.log(reversedLastHalf);
-    console.log(word.slice(0, halfWordNum));
-
-    if (reversedLastHalf == word.slice(0, halfWordNum)) {
-        return true;
-    } else {
-        return false;
-    }
+    console.log(word.split('').reverse().join(''));
+    return (word.split('').reverse().join('') == word);
 
 
 };
