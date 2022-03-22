01e177adbeb107644554bece3007960a7b147547
Jacob Brook <brookjacob@gmail.com>
2022-03-22 01:09:18 -0600

    Add js for solution 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    //let oldestPerson = people.sort((person1, person2) => (person1.yearOfDeath - person1.yearOfBirth) > (person2.yearOfDeath - person2.yearOfBirth) ? 1 : -1)[people.length - 1];
+    let ageCalculation = people.map(person => {
+        if (!person.yearOfDeath) {
+            let today = new Date();
+            return { name: `${person.name}`, age: today.getFullYear() - person.yearOfBirth }
+        } else {
+            return { name: `${person.name}`, age: person.yearOfDeath - person.yearOfBirth }
+        }
+    })
+    let sortedAgeCalculation = ageCalculation.sort((person1, person2) => person1.age > person2.age ? 1 : -1);
 
+    return people.filter(person => person.name == sortedAgeCalculation[sortedAgeCalculation.length - 1].name)[0];
 };
 
 // Do not edit below this line
