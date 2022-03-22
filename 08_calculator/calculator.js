41624872c460a1cf98ec7433ecebef2341848f61
Jacob Brook <brookjacob@gmail.com>
2022-03-21 23:28:45 -0600

    Update calculator.js functions with array.reduce()

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,23 +7,11 @@ const subtract = function(num1, num2) {
 };
 
 const sum = function(sumArray) {
-    let sumTotal = 0;
-
-    for (i = 0; i < sumArray.length; i++) {
-        sumTotal += sumArray[i];
-    }
-
-    return sumTotal;
+    return sumArray.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
 };
 
 const multiply = function(multiplyArray) {
-    let product = 1;
-
-    for (i = 0; i < multiplyArray.length; i++) {
-        product *= multiplyArray[i];
-    }
-
-    return product;
+    return multiplyArray.reduce((previousValue, currentValue) => previousValue * currentValue, 1);
 };
 
 const power = function(num1, num2) {
@@ -31,14 +19,10 @@ const power = function(num1, num2) {
 };
 
 const factorial = function(num1) {
-    let product = 1;
-
-    for (i = num1; i > 0; i--) {
-        product *= i;
-    }
-
-    return product;
-
+    let num1Array = [...Array(num1 + 1).keys()];
+    num1Array.shift()
+    console.log(num1Array);
+    return num1Array.reduce((previousValue, currentValue) => previousValue * currentValue, 1);
 };
 
 // Do not edit below this line
